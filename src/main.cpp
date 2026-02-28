332d4e91f97d27ed9d3d5def76510f7c43819e2a
lavany108 <lavanya.gla_cs.aiml24@gla.ac.in>
2026-02-28 17:17:22 +0530

    Added code to print distance in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -27,6 +27,8 @@ void loop() {
     } else {
         myServo.write(0);     // reset servo
     }
-
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
     delay(500);
 }
